aba84542a42adadb505de86f82cb11d5373a4b43
Daniel Driussi <daniel@Daniels-MacBook-Pro.local>
2015-08-08 19:37:05 -0300

    Initial Commit

---
+++ KickOffDemoCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KickOffDemoCode
+//
+//  Created by Daniel Driussi on 8/8/15.
+//  Copyright (c) 2015 Daniel Driussi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KickOffDemoCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KickOffDemoCode
+//
+//  Created by Daniel Driussi on 8/8/15.
+//  Copyright (c) 2015 Daniel Driussi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
